dd8c2247dfa5f7db9b02bca05851ecb40a364b15
PrettyMisaka <123646696+PrettyMisaka@users.noreply.github.com>
2023-09-21 20:16:04 +0800

    hdmi show ok (maybe

--- BSRAM_test/src/GreedySnake_hdmi.v
+++ BSRAM_test/src/GreedySnake_hdmi.v
@@ -37,8 +37,8 @@ reg  [3:0]   state;
 
 reg  [15:0] snake_map_arr_tmp;
 reg  [15:0]  x_list;
-reg  [3:0]   x_cnt;
-reg  [3:0]   y_cnt;
+reg  [4:0]   x_cnt;
+reg  [4:0]   y_cnt;
 
 wire [11:0]  O_h_cnt ;
 wire [11:0]  O_v_cnt ;
@@ -101,10 +101,11 @@ always@(posedge I_pxl_clk or negedge I_rst_n)begin
             HDMI_JUDGE:begin
                 if(O_h_cnt == 12'd800)begin
                     if(O_v_cnt == 12'd60 ) begin
-                        y_cnt <= y_cnt + 4'd1;
+                        y_cnt <= 5'd1;
                     end
                     else if(O_v_cnt == 12'd540 ) begin
                         vs_cmp <= 12'd60 + SNAKE_HSVS_STEP_CNT;
+                        y_cnt <= 5'd17;
                     end
                     else if(O_v_cnt == vs_cmp ) begin
                         y_cnt <= y_cnt + 4'd1;
@@ -117,34 +118,107 @@ always@(posedge I_pxl_clk or negedge I_rst_n)begin
                         state <= HDMI_FINISH;
                     end
                 end
+                /*********** hs cnt ****************/
+                if(O_h_cnt == 12'd159) begin
+                    x_cnt <= 5'd1;
+                end
+                else if(O_h_cnt == 12'd639 ) begin
+                    hs_cmp <= 12'd160 + SNAKE_HSVS_STEP_CNT;
+                    x_cnt <= 5'd17;
+                end
+                else if(O_h_cnt == hs_cmp - 5'd1) begin
+                    x_cnt <= x_cnt + 4'd1;
+                    hs_cmp<= hs_cmp+ SNAKE_HSVS_STEP_CNT;
+                end
+                else begin
+                    x_cnt <= x_cnt;
+                end
+                if(5'd1 <= y_cnt && y_cnt <= 5'd16)begin
+                    case (x_cnt)
+                        5'd1 : begin if(snake_map_arr_tmp & 16'b1000_0000_0000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd2 : begin if(snake_map_arr_tmp & 16'b0100_0000_0000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd3 : begin if(snake_map_arr_tmp & 16'b0010_0000_0000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd4 : begin if(snake_map_arr_tmp & 16'b0001_0000_0000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd5 : begin if(snake_map_arr_tmp & 16'b0000_1000_0000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd6 : begin if(snake_map_arr_tmp & 16'b0000_0100_0000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd7 : begin if(snake_map_arr_tmp & 16'b0000_0010_0000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd8 : begin if(snake_map_arr_tmp & 16'b0000_0001_0000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd9 : begin if(snake_map_arr_tmp & 16'b0000_0000_1000_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd10: begin if(snake_map_arr_tmp & 16'b0000_0000_0100_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd11: begin if(snake_map_arr_tmp & 16'b0000_0000_0010_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd12: begin if(snake_map_arr_tmp & 16'b0000_0000_0001_0000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd13: begin if(snake_map_arr_tmp & 16'b0000_0000_0000_1000)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd14: begin if(snake_map_arr_tmp & 16'b0000_0000_0000_0100)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd15: begin if(snake_map_arr_tmp & 16'b0000_0000_0000_0010)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        5'd16: begin if(snake_map_arr_tmp & 16'b0000_0000_0000_0001)begin 
+                                O_color <= SNAKE_COLOR; end
+                            else begin O_color <= POINT_COLOR; end end
+                        default: begin
+                            O_color <= BACKG_COLOR;
+                        end
+                    endcase
+                end
+                else begin
+                    O_color <= BACKG_COLOR;
+                end
             end
         endcase
     end
 end
 
 always@(posedge I_pxl_clk)begin
-    case(y_cnt):
-        4'd0 : begin snake_map_arr_tmp <= snake_map_arr_0 ; end
-        4'd1 : begin snake_map_arr_tmp <= snake_map_arr_1 ; end
-        4'd2 : begin snake_map_arr_tmp <= snake_map_arr_2 ; end
-        4'd3 : begin snake_map_arr_tmp <= snake_map_arr_3 ; end
-        4'd4 : begin snake_map_arr_tmp <= snake_map_arr_4 ; end
-        4'd5 : begin snake_map_arr_tmp <= snake_map_arr_5 ; end
-        4'd6 : begin snake_map_arr_tmp <= snake_map_arr_6 ; end
-        4'd7 : begin snake_map_arr_tmp <= snake_map_arr_7 ; end
-        4'd8 : begin snake_map_arr_tmp <= snake_map_arr_8 ; end
-        4'd9 : begin snake_map_arr_tmp <= snake_map_arr_9 ; end
-        4'd10: begin snake_map_arr_tmp <= snake_map_arr_10; end
-        4'd11: begin snake_map_arr_tmp <= snake_map_arr_11; end
-        4'd12: begin snake_map_arr_tmp <= snake_map_arr_12; end
-        4'd13: begin snake_map_arr_tmp <= snake_map_arr_13; end
-        4'd14: begin snake_map_arr_tmp <= snake_map_arr_14; end
-        4'd15: begin snake_map_arr_tmp <= snake_map_arr_15; end
+    case(y_cnt)
+        5'd1 : begin snake_map_arr_tmp <= snake_map_arr_0 ; end
+        5'd2 : begin snake_map_arr_tmp <= snake_map_arr_1 ; end
+        5'd3 : begin snake_map_arr_tmp <= snake_map_arr_2 ; end
+        5'd4 : begin snake_map_arr_tmp <= snake_map_arr_3 ; end
+        5'd5 : begin snake_map_arr_tmp <= snake_map_arr_4 ; end
+        5'd6 : begin snake_map_arr_tmp <= snake_map_arr_5 ; end
+        5'd7 : begin snake_map_arr_tmp <= snake_map_arr_6 ; end
+        5'd8 : begin snake_map_arr_tmp <= snake_map_arr_7 ; end
+        5'd9 : begin snake_map_arr_tmp <= snake_map_arr_8 ; end
+        5'd10: begin snake_map_arr_tmp <= snake_map_arr_9 ; end
+        5'd11: begin snake_map_arr_tmp <= snake_map_arr_10; end
+        5'd12: begin snake_map_arr_tmp <= snake_map_arr_11; end
+        5'd13: begin snake_map_arr_tmp <= snake_map_arr_12; end
+        5'd14: begin snake_map_arr_tmp <= snake_map_arr_13; end
+        5'd15: begin snake_map_arr_tmp <= snake_map_arr_14; end
+        5'd16: begin snake_map_arr_tmp <= snake_map_arr_15; end
     endcase
 end
 
 GreedySnake_hdmi_clk GreedySnake_hdmi_clk0(
-	.I_pxl_clk   (pxl_clk            ),//pixel clock
+	.I_pxl_clk   (I_pxl_clk          ),//pixel clock
     .I_en        (I_en               ),
     .I_rst_n     (I_rst_n            ),//low active 
     .I_h_total   (12'd1056           ),//hor total time  // 12'd1056  // 12'd1344  // 12'd1650   
