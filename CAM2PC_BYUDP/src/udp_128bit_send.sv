fc0262fd7c4b4844b0bf45855b31486f4285da6d
PrettyMisaka <123646696+PrettyMisaka@users.noreply.github.com>
2023-10-25 14:07:29 +0800

    udp 128bit finish

--- CAM2PC_BYUDP/src/udp_128bit_send.sv
+++ CAM2PC_BYUDP/src/udp_128bit_send.sv
@@ -36,8 +36,12 @@ enum logic[3:0] {
 //------------o_ddr3_data_upd_req
 logic [2:0] o_ddr3_data_upd_req_delay_buf;
 logic o_ddr3_data_upd_req_delay_val;
+logic [2:0] o_udp_frame_down_buf;
+logic o_udp_frame_down_val;
 assign o_ddr3_data_upd_req = o_ddr3_data_upd_req_delay_buf[2]|o_ddr3_data_upd_req_delay_buf[1]|
                             o_ddr3_data_upd_req_delay_buf[0]|o_ddr3_data_upd_req_delay_val;
+assign o_udp_frame_down = o_udp_frame_down_buf[2]|o_udp_frame_down_buf[1]|
+                            o_udp_frame_down_buf[0]|o_udp_frame_down_val;
 //------------i_udp_head_down
 logic i_udp_head_down_bef, i_udp_head_down_pos_flag;
 assign i_udp_head_down_pos_flag = (~i_udp_head_down_bef)&i_udp_head_down;
@@ -45,10 +49,12 @@ always@(posedge i_udp_clk50m or negedge i_rst_n)begin
     if(~i_rst_n)begin
         o_ddr3_data_upd_req_delay_buf <= 3'd0;
         i_udp_head_down_bef           <= 1'd0;
+        o_udp_frame_down_buf <= 3'd0;
     end
     else begin
         o_ddr3_data_upd_req_delay_buf <= {o_ddr3_data_upd_req_delay_buf[1:0],o_ddr3_data_upd_req_delay_val}
         i_udp_head_down_bef <= i_udp_head_down;
+        o_udp_frame_down_buf <= {o_udp_frame_down_buf[1:0],o_udp_frame_down_val}
     end
 end
 //------------------
@@ -57,6 +63,8 @@ end
 logic [127:0] wrdata_buf;
 logic [4:0] wrdata_byte_cnt;
 logic [5:0] udp_data_byte_cnt;
+logic [15:0] udp_jpeg_len;
+logic [15:0] udp_jpeg_data_cnt;
 assign o_udp_data_len = i_udp_jpeg_len + 16'd2;
 assign o_ipv4_sign = i_udp_ipv4_sign;
 assign o_udp_data = wrdata_buf[127:120];
@@ -64,8 +72,15 @@ task task_rst();
     state <= IDLE;
     o_udp_tx_en <= 1'd0;
     o_udp_tx_de <= 1'd0;
+
     wrdata_byte_cnt <= 5'd0;
+    udp_jpeg_len <= 16'd0;
+    udp_jpeg_data_cnt <= 16'd0
+
+    o_udp_tx_de <= 1'd0;
     o_ddr3_data_upd_req_delay_val <= 1'd0;
+    o_busy <= 1'd0;
+    o_udp_frame_down_val <= 1'd0;
 endtask
 initial task_rst();
 always@(posedge i_udp_clk50m or negedge i_rst_n)begin
@@ -78,6 +93,7 @@ always@(posedge i_udp_clk50m or negedge i_rst_n)begin
                 if(i_en)begin
                     state <= WAIT_BYTE_LOAD;
                     o_udp_tx_en <= 1'd1;
+                    o_busy <= 1'd1;
                 end
                     o_udp_tx_de <= 1'd0;
                     o_ddr3_data_upd_req_delay_val <= 1'd0;
@@ -86,6 +102,7 @@ always@(posedge i_udp_clk50m or negedge i_rst_n)begin
                 if(i_udp_head_down_pos_flag)begin
                     wrdata_buf <= {i_udp_last_frame_flag,i_mjpeg_frame_rank,112'd0};
                     state <= SIGN_BYTE_2;
+                    o_udp_tx_de <= 1'd1;
                 end
             end
             SIGN_BYTE_2:begin
@@ -94,6 +111,8 @@ always@(posedge i_udp_clk50m or negedge i_rst_n)begin
                     wrdata_byte_cnt <= 5'd0;
                     state <= WAIT_BYTE_LOAD;
                     o_ddr3_data_upd_req_delay_val <= 1'd0;
+                    udp_jpeg_len <= i_udp_jpeg_len;
+                    udp_jpeg_data_cnt <= 16'd0;
                 end
             end
             WAIT_BYTE_LOAD:begin
@@ -112,6 +131,18 @@ always@(posedge i_udp_clk50m or negedge i_rst_n)begin
                     else begin
                         wrdata_byte_cnt <= wrdata_byte_cnt + 5'd1;
                     end
+                    if(udp_jpeg_data_cnt == udp_jpeg_len - 16'd1)begin
+                        state <= FINISH;
+                        o_udp_frame_down_val <= 1'd1;
+                    end
+                    else
+                        udp_jpeg_data_cnt <= udp_jpeg_data_cnt + 16'd1;
+                end
+            end
+            FINISH:begin
+                if(i_udp_busy == 1'd0)begin
+                    o_busy <= 1'd0;
+                    state <= IDLE;
                 end
             end
         endcase
